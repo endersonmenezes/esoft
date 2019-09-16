7f709e6300baf1466ca125716bb640023171e727
end <enderson_mc@hotmail.com>
2019-09-15 23:39:30 -0300

    -- lista de atividades a serem implementadas

--- programacaooo/aep1/src/main/java/aep1/App.java
+++ programacaooo/aep1/src/main/java/aep1/App.java
@@ -3,5 +3,11 @@ package aep1;
 public class App {
     public static void main(String[] args) {
 
+//        TODO Criar Aluno
+//        TODO Criar Avaliação
+//        TODO Criar Conceito
+//        TODO Erros para Conceito ~validação da nota
+//        TODO Desejaria fazer em python <3
+//        TODO Fazer testes unitários nas variaveis e retornar erros em console
     }
 }
